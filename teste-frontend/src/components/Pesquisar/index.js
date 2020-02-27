1c9aea39559a8958e95005eea125caa2a3162429
Luciano Andrade <luciano.andrade1610@gmail.com>
2020-02-27 02:48:11 -0300

    resultado da busca

--- teste-frontend/src/components/Pesquisar/index.js
+++ teste-frontend/src/components/Pesquisar/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+
 import logo from "./img/acheVideos.png";
-import { Button, TextField } from "@material-ui/core";
+import youtube from "../../services/youtube";
+
 import "./styles.scss";
 
 function Pesquisar() {
+  const [term, setTerm] = useState("");
+ 
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  }  
+
+  async function handleSubmit () {
+    const response = await youtube.get("search", {
+      params: {
+        part:'snippet',
+        maxResults: 10,
+        key: 'AIzaSyDdu3OaB_m--RiVza76T_IBLhdzUhaaAkc',
+        q: term
+      }
+    });
+    console.log(response)    
+  }
+
   return (
     <section className="pesquisarContainer">
       <h2>
@@ -15,9 +36,13 @@ function Pesquisar() {
           className="search" 
           placeholder="Pesquisar..."
           variant="outlined"
+          value={term}
+          onChange={handleChange}
           fullWidth
         />      
-        <Button className="btnSearch" 
+        <Button
+          onClick={handleSubmit}
+          className="btnSearch" 
           variant="contained" 
           color="primary"
         >
